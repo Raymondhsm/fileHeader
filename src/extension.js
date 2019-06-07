5fe34793c5f111d8bb74321825b53d0532704fc8
Raymondhsm <1260307197@qq.com>
2019-06-07 20:09:01 +0800

    移动extension&在光标插入

--- src/extension.js
+++ src/extension.js
@@ -5,20 +5,27 @@ const vscode = require('vscode');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
+let window = vscode.window;
+let workspace = vscode.workspace;
+let env = vscode.env;
+let document = window.activeTextEditor.document;
+let editor = window.activeTextEditor;
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "fileHeader" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let fileHeader = vscode.commands.registerCommand('extension.fileHeader', function () {
-		// The code you place here will be executed every time your command is executed
+		let workspaceEdit = new vscode.WorkspaceEdit();
+
+		let line = editor.selection.active.line;
+
+		
+		workspaceEdit.insert(document.uri,new vscode.Position(line,0),"test");
+		workspace.applyEdit(workspaceEdit);
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('file');
