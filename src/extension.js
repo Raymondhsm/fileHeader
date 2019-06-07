ac53d06096af9b248f1617b0e3cc6d1b0f27cb56
Raymondhsm <1260307197@qq.com>
2019-06-08 01:55:15 +0800

    add time

--- src/extension.js
+++ src/extension.js
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const note = require("./noteStyle");
+const fs = require('fs');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,12 +21,20 @@ function activate(context) {
 	console.log('Congratulations, your extension "fileHeader" is now active!');
 
 	let fileHeader = vscode.commands.registerCommand('extension.fileHeader', function () {
+		// 文件创建时间
+		let time = new Date().toLocaleString()
+		try{
+			// 获取当前激活文件的路径
+			const filepath =editor.document.fileName;
+			time = new Date(fs.statSync(filepath).birthtime).toLocaleString()
+			
+			console.log(time)
+		}catch(err){
+			console.log(err.message)
+		}
+
 		let workspaceEdit = new vscode.WorkspaceEdit();
-
-		let line = editor.selection.active.line;
-
-		
-		workspaceEdit.insert(document.uri,new vscode.Position(line,0),"test");
+		workspaceEdit.insert(document.uri,new vscode.Position(0,0),note.noteData(time));
 		workspace.applyEdit(workspaceEdit);
 
 		// Display a message box to the user
@@ -33,6 +42,14 @@ function activate(context) {
 	});
 
 	let funHeader = vscode.commands.registerCommand("extension.funHeader", function() {
+		let workspaceEdit = new vscode.WorkspaceEdit();
+
+		let line = editor.selection.active.line;
+
+		
+		workspaceEdit.insert(document.uri,new vscode.Position(line,0),note.noteData());
+		workspace.applyEdit(workspaceEdit);
+
 
 		vscode.window.showInformationMessage('fun');
 	});
--- src/noteStyle.js
+++ src/noteStyle.js
@@ -1,5 +1,19 @@
 
 
-const noteData = () =>{
-    return "//Author: xiaoming \n description: test"
+const noteData = (time) =>{
+    let data = "/* \n"
+    data += " * Author:      xiaoming \n"
+    data += " * CreateTime:  "+ time +" \n"
+    data += " * LastEditor:  xiaoming \n"
+    data += " * LastTime:    "+ new Date().toLocaleString() +" \n"
+    data += " * Description: "
+    data += "*/ \n"
+
+    console.log(data)
+    return data
+}
+
+module.exports = {
+    noteData
+
 }
